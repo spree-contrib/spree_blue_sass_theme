99a25f905f5732691ca47993aad3cdaa9806dc87
Tyler Rick <tyler.rick@k3integrations.com>
2013-01-30 18:54:13 -0800

    Fix to look good with Spree 1.3

--- app/overrides/add_class_to_link_to_cart.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_store_menu',
-                     :name => 'add_class_to_link_to_cart',
-                     :set_attributes => '#link-to-cart[data-hook]',
-                     :attributes => { :class => 'cart-indicator' })
-
